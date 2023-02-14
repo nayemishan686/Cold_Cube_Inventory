7f2af701ff3f834231e81ef7db9080982784a55e
Nayem Ishan <nayemishan377@gmail.com>
2023-02-14 23:00:28 +0600

    Cold cube gitignore set

--- public/index.php
+++ public/index.php
@@ -46,17 +46,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// $install1 = is_dir('public/install');
-// $install2 = is_dir('install');
-
-// if ($install1 == true) {
-//     header("location:public/install/index.php");
-// }
-// elseif($install2 == true)
-// {
-//     header("location:install/index.php");
-// }
-// else {
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
@@ -64,4 +53,4 @@ $response = tap($kernel->handle(
 ))->send();
 
 $kernel->terminate($request, $response);
-// }
+
